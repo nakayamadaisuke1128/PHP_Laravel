397b654b52dd46b951f71a81c00d267fa16788bd
”nakayama_daisuke” <nd00001128@gmail.com>
2019-10-23 05:56:18 +0000

    test

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,49 +1,84 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
-
-
-//4.【応用】 artisanを使って、Admin/ProfileControllerを作成しましょう。
-    //php artisan make:controller Admin/ProfileController
-
-//5.【応用】 Admin/ProfileControllerに、以下のadd, create, edit, update それぞれのActionを追加してみましょう。
 {
-    //
     public function add()
     {
         return view('admin.profile.create');
     }
+    
+    
     public function create(Request $request)
     {
-        return redirect('admin/profile/create');
-        
-        $validatedData = $request->validate([
-            'name' => 'required',
-            'gender' => 'required',
-            'hobby' => 'required',
-            'introduction' => 'required',
-        ]);
+        // Varidationを行う
+      $this->validate($request, Profile::$rules);
 
-        $profile = new Profile();
-        $profile->name = $validatedData['name'];
-        $profile->gender = $validatedData['gender'];
-        $profile->hobby = $validatedData['hobby'];
-        $profile->introduction = $validatedData['introduction'];
-        
-        $profile->save();
-        return redirect('admin/profile/create');
+      $profile = new Profile;
+      $form = $request->all();
+     
+     unset($form['_token']);
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+      
+
+      return redirect('admin/profile/create');
     }
-    public function edit()
+    
+    
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのプロフィールを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
     }
-    public function update()
+    
+    
+    public function edit(Request $request)
     {
-        return redirect('admin/profile/edit');
+        $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+    
+    
+    public function update(Request $request)
+    {
+        // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
+    }
+    
+    
+    
+    
+    
+    public function delete(Request $request)
+    {
+      // 該当するProfile Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::group(['prefix' => 'admin'], function(){
+Route::group(['prefix' => 'admin'], function() {
 Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-Route::post('news/create', 'Admin\NewsController@create');
+Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
+
+
 
 Route::get('admin/profile/create', 'Admin\ProfileController@add')->middleware('auth');
 Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-Route::post('admin/profile/create', 'Admin\ProfileController@create');
-Route::post('admin/profile/edit', 'Admin\ProfileController@update');
+Route::post('admin/profile/create', 'Admin\ProfileController@create')->middleware('auth');
+Route::post('admin/profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+Route::get('admin/profile', 'Admin\ProfileController@index')->middleware('auth');
+Route::get('admin/profile/delete', 'Admin\ProfileController@delete')->middleware('auth');
 
 
 Auth::routes();

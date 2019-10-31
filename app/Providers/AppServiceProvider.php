2a4ad6163352bd23a38a954f1eb11043e9b72356
”nakayama_daisuke” <nd00001128@gmail.com>
2019-10-31 14:03:18 +0000

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
+    // 以下を追記
+    if (\App::environment('production')) {
+        \URL::forceScheme('https');
         }
     }
 }
